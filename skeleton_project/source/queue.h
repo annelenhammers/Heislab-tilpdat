36c742e218083ada2c6ae6e6dc2ca9dd806b95fa
Ann Elen <annelenhammersland@gmail.com>
2024-02-23 18:39:51 +0100

    started working on queue

---
+++ skeleton_project/source/queue.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "floor.h"
+
+
+typedef enum {
+    UP = 0,
+    DOWN = 1,
+    
+}QueueDirection;
+
+typedef struct{
+    QueueDirection queuedirection;
+    Floor floor;
+    QueueState* prevoius;
+}QueueState;
+
+typedef struct {
+    int size;
+    QueueState* prevoius;
+    QueueState* head;
+    QueueState* tail;
+    QueueState* next;
+}Queue;
+
+QueueState* Queuestate(QueueDirection queuedirection, Floor floor);
+void DestoryQueue(Queue* queue);
+void clearQueue(Queue* queue);
+
