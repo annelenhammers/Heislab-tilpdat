3e9b85637c1101312eda0e33388cbe7a4066c8e9
maya <mayasaintvictor@gmail.com>
2024-02-26 19:39:36 +0100

    uunsure if its necessary but this file will work more directly with hardware

---
+++ skeleton_project/source/hardware_interface.c
@@ -0,0 +1,5 @@
+/*denne modulen skal inneholde:
+timer() funksjoner - først og fremst lage en "klokke", så bruke denne 
+for timer til f.eks open door funksjonen
+check() funksjoner - floor, obstruction og ikke minst check_button_status()
+*/
