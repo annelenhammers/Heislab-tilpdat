91910d680763164744809d5cc86f84ed05ebb0af
maya <mayasaintvictor@gmail.com>
2024-02-26 19:30:26 +0100

    made a panel file

--- skeleton_project/source/panel_control.c
+++ skeleton_project/source/panel_control.c
@@ -3,7 +3,12 @@
 
 #include "../source/driver/elevio.h"
 
+//Kommentar
 //det burde sikkert ikke være vanlig int
+
+
+//FLOOR PANEL
+
 int floor_button_up[N_FLOORS] = {0};
 int floor_button_down[N_FLOORS] = {0};
 int floor_lights[N_FLOORS] = {0};
@@ -11,4 +16,10 @@ int floor_lights[N_FLOORS] = {0};
 void init_floor(){}
 void update_floor_button(){}
 void update_floor_lights(){}
+
+//ELEVATOR PANEL
+
+int elevator_buttons[N_FLOORS] = {0};
+
+//update elevator button function
 
